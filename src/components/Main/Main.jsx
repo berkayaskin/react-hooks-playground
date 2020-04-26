6b260b09150f8c895563c89f17d36aa733295ef4
berkayaskin <berkay.askin@bitechnology.com>
2020-04-26 07:21:05 +0300

    solved EX-2

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,38 +1,35 @@
-import React, { Component } from 'react';
-import { MainWrapper, Button, List, ListItem } from './Main.style';
+import React, {useState} from 'react';
+import {MainWrapper, Button, List, ListItem} from './Main.style';
 
-class Main extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            firstName: 'Joe',
-            lastName: 'Smith',
-            age: 38
-        };
-    }
+function Main() {
 
-    changeName = () => {
-        this.setState({
+    const [credentials, setCredentials] = useState({
+        firstName: 'Joe',
+        lastName: 'Smith',
+        age: 38
+    });
+
+    const changeName = () => {
+        setCredentials(prevState => ({
+            ...prevState,
             firstName: 'Steve'
-        });
-    };
-
-    render() {
-        return (
-            <MainWrapper>
-                <h2>EX2_useState_intermediate</h2>
-                <p>my credentials are:</p>
-                <List>
-                    {Object.keys(this.state).map(credential => (
-                        <ListItem>
-                            {`${credential}: ${this.state[credential]}`}
-                        </ListItem>
-                    ))}
-                </List>
-                <Button onClick={this.changeName}>CHANGE NAME</Button>
-            </MainWrapper>
-        );
+        }))
     }
+
+    return (
+        <MainWrapper>
+            <h2>EX2_useState_intermediate</h2>
+            <p>my credentials are:</p>
+            <List>
+                {Object.keys(credentials).map(credential => (
+                    <ListItem>
+                        {`${credential}: ${credentials[credential]}`}
+                    </ListItem>
+                ))}
+            </List>
+            <Button onClick={changeName}>CHANGE NAME</Button>
+        </MainWrapper>
+    );
 }
 
 export default Main;
